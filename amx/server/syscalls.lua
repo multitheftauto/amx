edab65806b4c3a9716cf42a189832f56607b3228
Konstantin <apnkel@gmail.com>
2020-06-21 15:14:06 +0300

    new fixes from comments

--- amx/server/syscalls.lua
+++ amx/server/syscalls.lua
@@ -2765,53 +2765,59 @@ end
 
 -- Now we have all of RESTful types of requests. Our function is better!
 -- The SAMP documentation said about 'url' - "The URL you want to request. (Without 'http://')"
--- But I didn't set it up. Because WE SUPPORT HTTPS!
+-- I made a check. The state without a protocol is called as 'default'.
+-- HTTP and HTTPS you can put into URL if you want. It works fine.
 -- TODO: An "index" argument only for compatibility.
 function HTTP(amx, index, type, url, data, callback)
 
+	local protomatch = pregMatch(url,'^(\\w+):\\/\\/')
+	local proto = protomatch[1] or 'default'
+	-- if somebody will try to put here ftp:// ssh:// etc...
+	if proto ~= 'http' and proto ~= 'https' and proto ~= 'default' then
+		print('Current protocol is not supporting')
+		return 0
+	end
 	local typesToText = {
 		'GET',
 		'POST',
 		'HEAD',
-		'PUT',
-		'PATCH',
-		'DELETE',
-		'COPY',
-		'OPTIONS',
-		'LINK',
-		'UNLINK',
-		'PURGE',
-		'LOCK',
-		'UNLOCK',
-		'PROPFIND',
-		'VIEW'
+		[-4] = 'PUT',
+		[-5] = 'PATCH',
+		[-6] = 'DELETE',
+		[-7] = 'COPY',
+		[-8] = 'OPTIONS',
+		[-9] = 'LINK',
+		[-10] = 'UNLINK',
+		[-11] = 'PURGE',
+		[-12] = 'LOCK',
+		[-13] = 'UNLOCK',
+		[-14] = 'PROPFIND',
+		[-15] = 'VIEW'
 	}
 	local sendOptions = {
-		queueName = "A SAMP request queue",
+		queueName = "amx." .. getResourceName(amx.res) .. "." .. amx.name,
 		postData = data,
 		method = typesToText[tonumber(type)],
 	}
-	fetchRemote(url, sendOptions, 
+	local successRemote = fetchRemote(url, sendOptions, 
 	function (responseData, responseInfo)
 		local error = responseInfo.statusCode
 		if error == 0 then
 			procCallInternal(amx, callback, index, 200, responseData)
-			return 1;
 		elseif error >= 1 and error <= 89 then
 			procCallInternal(amx, callback, index, 3, responseData)
-			return 0;
 		elseif error == 1006 or error == 1005 then
 			procCallInternal(amx, callback, index, 1, responseData)
-			return 0;
 		elseif error == 1007 then 
 			procCallInternal(amx, callback, index, 5, responseData)	
-			return 0;
 		else
 			procCallInternal(amx, callback, index, error, responseData)
-			return 0;
 		end
 	end)
-	return 0;
+	if not success then
+		return 0
+	end
+	return 1
 end
 
 function Create3DTextLabel(amx, text, r, g, b, a, x, y, z, dist, vw, los)
