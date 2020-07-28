0824b7a0a212f23c1c14e78a3f8f366f8e7d5dea
[BR]John_Magdy <19735243+Zorono@users.noreply.github.com>
2020-07-28 06:39:45 +0200

    Some kind of...

--- amx/server/natives/a_vehicles.lua
+++ amx/server/natives/a_vehicles.lua
@@ -35,7 +35,7 @@ function SetVehicleParamsForPlayer(amx, vehicle, player, isObjective, doorsLocke
 end
 
 
-function ManualVehicleEngineAndLights() 
+function ManualVehicleEngineAndLights()
 	ManualVehEngineAndLights = true
 end
 
@@ -200,7 +200,7 @@ end
 -- 11 - CARMODTYPE_REAR_BUMPER
 -- 12 - CARMODTYPE_VENT_RIGHT
 -- 13 - CARMODTYPE_VENT_LEFT
-function GetVehicleComponentType(amx, componentid) 
+function GetVehicleComponentType(amx, componentid)
 	local components = {
 		['Spoiler'] = 0,
 		['Hood'] = 1,
@@ -234,17 +234,17 @@ function RepairVehicle(amx, vehicle)
 	fixVehicle(vehicle)
 end
 
-function GetVehicleVelocity(amx, vehicle, refVX, refVY, refVZ)
-	local vx, vy, vz = getElementVelocity(vehicle)
-	writeMemFloat(amx, refVX, vx)
-	writeMemFloat(amx, refVY, vy)
-	writeMemFloat(amx, refVZ, vz)
-end
+-- function GetVehicleVelocity(amx, vehicle, refVX, refVY, refVZ)
+-- 	local vx, vy, vz = getElementVelocity(vehicle)
+-- 	writeMemFloat(amx, refVX, vx)
+-- 	writeMemFloat(amx, refVY, vy)
+-- 	writeMemFloat(amx, refVZ, vz)
+-- end
 
-function SetVehicleVelocity(amx, vehicle, vx, vy, vz)
-	setElementVelocity(vehicle, vx, vy, vz)
+-- function SetVehicleVelocity(amx, vehicle, vx, vy, vz)
+--	setElementVelocity(vehicle, vx, vy, vz) \
 	--setElementAngularVelocity(vehicle, vx, vy, vz) --This isn't needed, it makes the car spin and I believe samp doesn't do this
-end
+-- end
 
 function GetVehicleDamageStatus(amx, vehicle, refPanels, refDoors, refLights, refTires)
 	local panelsState = getVehiclePanelState(vehicle, 0)
